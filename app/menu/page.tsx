45885fed809db15c63cd44c107c16d374e6d13dd
veskoap <veskoap@gmail.com>
2023-12-30 14:10:28 -0500

    capitalization changes

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -13,10 +13,10 @@ export default function Page() {
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-            Menu coming soon
+            Menu Coming Soon
           </h2>
           <p className="mt-4 text-zinc-400">
-            Expect our menu to drop March 2024.
+            Expect Menu Drop March 2024.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
