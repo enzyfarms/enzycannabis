7bff8d6bd4c8c80f0dcec1bcf2878e68a7d1582e
VesselinPortev <154934344+VesselinPortev@users.noreply.github.com>
2024-09-22 20:07:38 -0400

    Update page.tsx

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Script from "next/script";
 import { Navigation } from "../components/nav";
 import { Redis } from "@upstash/redis";
 import { Eye } from "lucide-react";
@@ -13,13 +14,17 @@ export default function Page() {
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-            Menu Coming Soon
+            Our Menu
           </h2>
           <p className="mt-4 text-zinc-400">
             Expect Menu Drop March 2024.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
+        <Script
+          src="https://enzyfarms.wm.store/static/js/embed.js"
+          strategy="afterInteractive"
+        />
       </div>
     </div>
   );
