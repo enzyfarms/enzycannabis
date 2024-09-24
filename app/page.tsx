a12768f6b446853ec37fe0fa92bf8957e2b4ec15
VesselinPortev <154934344+VesselinPortev@users.noreply.github.com>
2024-09-24 17:32:25 -0400

    Update /page.tsx for quicker Large Contentful Paint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import Particles from "./components/particles";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
+
+// Dynamically load the Particles component to reduce initial load
+const Particles = dynamic(() => import("./components/particles"), {
+  ssr: false, // Disable server-side rendering for Particles
+  loading: () => <div />, // Display nothing while loading
+});
 
 const navigation = [
   { name: "Menu", href: "/menu" },
@@ -11,6 +17,9 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      {/* Preload critical resources */}
+      <link rel="preload" href="/Logo.png" as="image" />
+
       <nav className="z-10 my-8 md:my-16 animate-fade-in">
         <ul className="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-4">
           {navigation.map((item) =>
@@ -34,8 +43,14 @@ export default function Home() {
           )}
         </ul>
       </nav>
+
+      {/* Glow animation */}
       <div className="w-screen h-px animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 animate-fade-left" />
-      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
+
+      {/* Reduced particle effect quantity */}
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={50} />
+
+      {/* Priority image loading */}
       <h1 className="z-10 text-3xl sm:text-4xl md:text-6xl lg:text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text">
         <Image
           src="/Logo.png"
@@ -43,9 +58,12 @@ export default function Home() {
           height={398}
           alt="Logo"
           className="w-32 h-auto sm:w-48 md:w-64 lg:w-96"
+          priority // Ensure image loads with higher priority
         />
       </h1>
+
       <div className="w-screen h-px animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 animate-fade-right" />
+
       <div className="my-8 md:my-16 text-center animate-fade-in">
         <h2 className="text-sm md:text-base text-zinc-500"></h2>
       </div>
